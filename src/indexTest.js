f610bac1ac34fc3a5c130b4b1de10b0cc5da43d4
antheboets <anthe.boets@student.ehb.be>
2022-07-05 00:45:28 +0200

    staging more temp code

--- src/indexTest.js
+++ src/indexTest.js
@@ -3,12 +3,20 @@ import {FontLoader} from './lib/FontLoader'
 import {TextGeometry} from './lib/TextGeometry'
 import {OrbitControls} from './lib/OrbitControls'
 
+()=>{
+    console.log("test")
+}
+
 const mainFunc =  async ()=>{
     const setCanvasToScreen = (renderer) =>{
         //console.log(window.innerHeight,window.innerWidth)
         renderer.setSize( window.innerWidth, window.innerHeight );
     }
-    
+    /*
+    Consumer
+    SoftProducts
+    The Authority on Life...
+    */
     const scene = new Scene();
     scene.background = new Color(0xffffff)
     const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -24,19 +32,20 @@ const mainFunc =  async ()=>{
     controls.update()
 
     const fontLoader = new FontLoader();
-    fontLoader.load('./fonts/test3.json',(font)=>{
-        console.log(font)
-        const text = new TextGeometry( "consumersoft", {
+    fontLoader.load('./fonts/MingLiU_Regular3.json',(font)=>{
+        console.log(font,"font")
+        //Consumer\nSoftProducts\nThe Authority on Life...
+        const text = new TextGeometry("Consumer\nSoftProducts\nThe Authority on Life...", {
             font: font,
             size: 50,
             height: 10,
         });
+        console.log(text,"text")
         const textMat = new MeshBasicMaterial({color: 0x00ff00});
-        console.log(font)
         const textMesh = new Mesh(text,textMat)
-        console.log(font)
         scene.add(textMesh)
     })
+
     /*
     const font = await fontLoader.loadAsyn('./fonts/MingLiU_Regular.json',(font)=>{console.log(font)})
     console.log(font)
